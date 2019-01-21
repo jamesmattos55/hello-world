87e18d1daf7437c16184b5a0611fcae99cd09e50
James Mattos <jamesmattos55@yahoo.com>
2019-01-21 16:28:32 -0700

    Project with file implementing "Hello World!" program.

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * Class on displaying "Hello World!" message.
+ */
+public class HelloWorld {
+
+  /**
+   * Prints "Hello World!" to a standard output.
+   * 
+   * @param args    Command-lines parameters (ignored)
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
